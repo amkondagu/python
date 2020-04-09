2d3d563930ff1d776a5fe466f1e08258d77a8153
amkondagu <57330699+amkondagu@users.noreply.github.com>
2020-04-08 18:28:22 -0700

    Add files via upload

--- GUI_Connect_4.py
+++ GUI_Connect_4.py
@@ -1,6 +1,9 @@
+import random
 import pygame
+from pygame import gfxdraw
 pygame.init()
-squareSize = 100
+player = random.choice(['X', 'O'])
+squareSize = 50
 width = 7*squareSize
 height = 6*squareSize+squareSize
 size = (width, height)
@@ -8,19 +11,22 @@ window = pygame.display.set_mode(size)
 blue = (0, 0, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
+red = (225, 0, 0)
 yellow = (255, 255, 0)
 pygame.draw.rect(window, blue, (0,squareSize,width,height-squareSize))
 pygame.display.set_caption('Connect 4')
-
+#print('heelo')
 import sys
 
 #invalid = True
 #sys.exit()
 #print("\nWrite 'q' to quit")
 board = []
+board = [['-' for row in range(7)] for i in range(6)]
+'''
 for row in range(6):
     board.append(['-', '-', '-','-', '-', '-', '-'])
+'''
 
 def drawBoard(bo):
     for row in range(len(bo)):
@@ -36,12 +42,14 @@ def drawBoard(bo):
             x = int(squareSize/2 + sqr*squareSize)
             y = int(squareSize/2 + squareSize*row + squareSize)
             #print(x)
-            pygame.draw.circle(window, color, (x, y), int(squareSize*0.4))
+            #pygame.draw.circle(window, color, (x, y), int(squareSize*0.4))
+            circle = pygame.gfxdraw.filled_circle(window, x, y, int(squareSize*0.4), color)            
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            
 
-player = 'X'
+
 def changePlayer():
     global player
     if player == 'X':
@@ -55,6 +63,7 @@ def place(move):
     for row in range(len(board)):
         if board[-1*(row+1)][move-1] == '-':
             board[-1*(row+1)][move-1] = player
+            changePlayer()
             invalid = False
             break
 def turn(person):
@@ -79,10 +88,9 @@ def turn(person):
             continue
         else:
             break
-
             '''
     place(move)
-    changePlayer()
+    
 def checkH():
     global board
     check = []
@@ -167,6 +175,18 @@ def main():
 game_over = False
 run = True
 while run:
+    window.fill(black)
+    if player == 'X':
+        color = red
+        #Awindow.fill(red)
+    elif player == 'O':
+        color = yellow
+        #window.fill(yellow)
+    #window.fill(color)
+    pygame.draw.rect(window, blue, (0, squareSize,width, height-squareSize))
+    pos = pygame.mouse.get_pos()
+    posx = pos[0]
+    pygame.gfxdraw.filled_circle(window, posx, squareSize//2, int(squareSize*0.4), color)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -174,6 +194,9 @@ while run:
             pos = pygame.mouse.get_pos()
             turn(player)
             #print(pos)
+
+         
+            
     drawBoard(board)
     pygame.display.update()
     if checkWin():
@@ -188,7 +211,8 @@ while run:
             player = 'Yellow'
         elif player == 'O':
             player = 'Red'
-        font = pygame.font.Font('freesansbold.ttf', 70)
+        pygame.draw.rect(window, black, (0,0,width, squareSize))
+        font = pygame.font.Font('freesansbold.ttf', int(squareSize))
         text = font.render(player+' wins!!', True, white, black)
         textRect = text.get_rect()
         textRect.center = (width//2, squareSize//2)
@@ -202,6 +226,23 @@ while run:
     #if invalid:       
  #       print('\nYou cannot go there!', end='')
         #continue
+    for row in board:
+        if '-' not in row:
+            tie = True 
+            
+        else:
+            tie = False
+            break
+        if tie:
+            pygame.draw.rect(window, black, (0,0,width, squareSize))
+            font = pygame.font.Font('freesansbold.ttf', int(squareSize))
+            text = font.render("It's a tie", True, white, black)
+            textRect = text.get_rect()
+            textRect.center = (width//2, squareSize//2)
+            window.blit(text, textRect)
+            pygame.display.update()
+            run = False
+            game_over = True
 while game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
